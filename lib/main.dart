30ff192be915b2d339644acd49d4d7dbf4f59a3d
Lauren Go <gtjy55@gmail.com>
2026-07-24 11:21:26 +0900

    home calendar

--- lib/main.dart
+++ lib/main.dart
@@ -17,11 +17,11 @@ class MyHomePage extends StatelessWidget {
     int _currentYear = DateTime.now().year;
     int _currentMonth = DateTime.now().month;
 
-    int padding = DateTime(_currentYear, _currentMonth, 1).weekday - 1;
+    int padding = DateTime(_currentYear, _currentMonth, 1).weekday % 7;
     int daysInMonth = DateTime(_currentYear, _currentMonth + 1, 0).day;
     return Scaffold(
       appBar: AppBar(
-        title: Text('Calendar'),
+        title: Text('$_currentMonth 월'),
         centerTitle: true,
         elevation: 0.0,
       ),
@@ -29,9 +29,13 @@ class MyHomePage extends StatelessWidget {
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 7,
           ),
-        itemCount: 35,
+        itemCount: 42,
         itemBuilder: (context, index) {
-          if(index <padding) return Container();
+          int actualDay = index - padding + 1;
+          
+          if(index <padding || actualDay > daysInMonth) return Container();
+
+          return Container(child: Text('$actualDay'),);
         },
       ),
     );

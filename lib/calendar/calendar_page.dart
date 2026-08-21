398a88092b8868e14d66abe45f98e4474e4b7281
Lauren Go <gtjy55@gmail.com>
2026-08-21 14:38:37 +0900

    fix 변수명 변경 displayDate, DataUtils 사용으로 change Month 함수 제거

--- lib/calendar/calendar_page.dart
+++ lib/calendar/calendar_page.dart
@@ -9,23 +9,12 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarState extends State<CalendarPage> {
-  DateTime displayedDate = DateTime(2026, 8, 1);
+  DateTime displayDate = DateTime(2026, 8, 1);
   DateTime? selectedDate;
-
-  void changeDisplayedMonth(int monthOffset) {
-    setState(() {
-      DateTime tempDate = DateTime(
-        displayedDate.year,
-        displayedDate.month + monthOffset,
-        displayedDate.day,
-      );
-      displayedDate = tempDate;
-    });
-  }
 
   void handleDaySelected(int dayNumber){
     setState(() {
-      selectedDate = DateTime(displayedDate.year, displayedDate.month, dayNumber);
+      selectedDate = DateTime(displayDate.year, displayDate.month, dayNumber);
     });
 
     showModalBottomSheet(context: context, builder: (BuildContext context){
@@ -43,30 +32,27 @@ class _CalendarState extends State<CalendarPage> {
     });
   }
 
-  void goToPreviousMonth()=>changeDisplayedMonth(-1);
-  void goToNextMonth() => changeDisplayedMonth(1);
-
   @override
   Widget build(BuildContext context) {
-    int displayedYear = displayedDate.year;
-    int displayedMonth = displayedDate.month;
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          onPressed: goToPreviousMonth,
+          onPressed: ()=>setState(() {
+            displayDate = DateUtils.addMonthsToMonthDate(displayDate, -1);
+  }),
           icon: Icon(Icons.keyboard_arrow_left),
         ),
-        title: Text('$displayedYear년 $displayedMonth월'),
+        title: Text('${displayDate.year}년 ${displayDate.month}월'),
         actions: <Widget>[
           IconButton(
-            onPressed: goToNextMonth,
+            onPressed: ()=>setState(() {displayDate = DateUtils.addMonthsToMonthDate(displayDate, 1);}),
             icon: Icon(Icons.keyboard_arrow_right),
           ),
         ],
         centerTitle: true,
         elevation: 0.0,
       ),
-      body: calendarWidget(showDate: displayedDate, onDaySelected: handleDaySelected, selectedDate: selectedDate,)
+      body: CalendarWidget(showDate: displayDate, onDaySelected: handleDaySelected, selectedDate: selectedDate,)
     );
   }
 }
